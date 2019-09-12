2c416cd35407e7feae4850eb7f8ac1454f4efc5d
confusdcodr <confusdcodr@users.noreply.github.com>
2019-09-11 20:39:05 -0400

    Upgrade to terraform 0.12.x

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
-provider "aws" {}
+provider "aws" {
+}
 
 resource "aws_iam_saml_provider" "this" {
-  count = "${var.create_saml_provider ? 1 : 0}"
+  count = var.create_saml_provider ? 1 : 0
 
-  name                   = "${var.saml_provider_name}"
-  saml_metadata_document = "${var.saml_provider_metadata}"
+  name                   = var.saml_provider_name
+  saml_metadata_document = var.saml_provider_metadata
 }
--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,5 @@
 output "saml_identity_provider_arn" {
   description = "ARN of the SAML Identity Provider"
-  value       = "${join("", aws_iam_saml_provider.this.*.arn)}"
+  value       = join("", aws_iam_saml_provider.this.*.arn)
 }
+
